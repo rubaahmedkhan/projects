72fff58a45482628109c76b40b4d650f2e1df0cc
rubaahmedkhan <160300382+rubaahmedkhan@users.noreply.github.com>
2024-02-25 07:32:17 +0500

    Add files via upload
    add projects

---
+++ Digital Clock/digital_clock.ts
@@ -0,0 +1,25 @@
+function showTime(){
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let session = "AM";
+
+    if(h>12) {
+        h = h-12;
+        //h = 13 - 12 = 1
+    }
+    // AM PM logic
+if(h>=12) {
+    session = "PM";
+
+}
+    h = h < 10 ? "0"+h : h;
+    m = m< 10 ? "0"+m : m;
+    s = s < 10 ? "0"+s : s;
+
+let time = h + " : " +  m + " : " + s + " "  + session;
+document .getElementsByTagName('h1')[0].innerText = time;
+
+    setTimeout(showTime,1000);
+}
